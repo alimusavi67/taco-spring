5df3ecfa2a22750a16b598316802c552d41b9ae4
ali_mousavi <alimusavi67@gmail.com>
2018-11-25 23:37:12 +0330

    tacos

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,23 @@
+package tacos;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,30 @@
+package tacos;
+import java.util.Date;
+import java.util.List;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+public class Taco {
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    @ManyToMany(targetEntity=Ingredient.class)
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Date createdAt;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+}
